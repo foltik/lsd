b3909b30e0fa7b38bfa895ef6edbb6b17e7909fe
RileyChampion <rchamp2u@gmail.com>
2025-04-20 13:00:17 -0700

    more changes to overview dashboard, added users dashboard, styling to widgets

--- src/app/admin.rs
+++ src/app/admin.rs
@@ -15,15 +15,26 @@ use crate::{
 
 /// Add all `admins` routes to the router.
 pub fn routes() -> AppRouter {
-    AppRouter::new().route("/dashboard", get(admin_dashboard))
+    AppRouter::new()
+        .route("/dashboard/overview", get(admin_overview_dashboard))
+        .route("/dashboard/users", get(admin_users_dashboard))
 }
 
-/// Display admin dashboard
-async fn admin_dashboard(
+/// Display admin overview dashboard
+async fn admin_overview_dashboard(State(state): State<SharedAppState>) -> AppResult<impl IntoResponse> {
+    let q = ListUserQuery { page: 0, page_size: 25 };
+
+    let users_count = User::list(&state.db, &q).await?.len();
+
+    Ok(views::admin::AdminDashboardOverview { users_count })
+}
+
+//
+async fn admin_users_dashboard(
     State(state): State<SharedAppState>,
     Query(query): Query<ListUserQuery>,
 ) -> AppResult<impl IntoResponse> {
     let users = User::list(&state.db, &query).await?;
 
-    Ok(views::admin::AdminDashboard { users })
+    Ok(views::admin::AdminDashboardUsersView { users })
 }
--- src/views/admin.rs
+++ src/views/admin.rs
@@ -3,7 +3,13 @@ use askama::Template;
 use askama_web::WebTemplate;
 
 #[derive(Template, WebTemplate)]
-#[template(path = "admin/dashboard.html")]
-pub struct AdminDashboard {
+#[template(path = "admin/dashboard/overview.html")]
+pub struct AdminDashboardOverview {
+    pub users_count: usize,
+}
+
+#[derive(Template, WebTemplate)]
+#[template(path = "admin/dashboard/users.html")]
+pub struct AdminDashboardUsersView {
     pub users: Vec<UserView>,
 }
