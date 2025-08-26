c3aa397b0d3778b0eaf8151f83853be53d914c32
Sam Wlody <swlody@gmail.com>
2025-08-26 17:53:25 -0400

    most wip

--- src/app/bulletin.rs
+++ src/app/bulletin.rs
@@ -1,28 +1,32 @@
-use axum::routing::{post, put};
+use axum::routing::post;
 use axum::Json;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
 use crate::prelude::*;
 
+// TODO(sam) ensure minimal data serialization
+// TODO(sam) implement remove-after logic for remove_after_time
+// TODO(sam) honestly maybe disable moving the flyers at all?
+// TODO(sam) event description etc that shows when the flyer is clicked?
+// TODO(sam) event URL that links to the event details?
 #[derive(Debug, Serialize, Deserialize)]
 struct Flyer {
     id: i64,
+    user_id: i64,
+    flyer_name: Option<String>,
     x: i64,
     y: i64,
     rotation: i64,
-    z_index: i64,
     image_url: String,
-    event_url: Option<String>,
-    event_end: NaiveDateTime,
+    remove_after_time: NaiveDateTime,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct FlyerForm {
+struct CreateFlyerForm {
     image_url: String,
-    event_name: Option<String>,
-    event_url: Option<String>,
-    event_end: NaiveDateTime,
+    flyer_name: Option<String>,
+    remove_after_time: NaiveDateTime,
     x: i64,
     y: i64,
 }
@@ -30,42 +34,45 @@ struct FlyerForm {
 async fn create_flyer(
     user: User,
     State(state): State<SharedAppState>,
-    Form(flyer): Form<FlyerForm>,
-) -> AppResult<()> {
+    Form(flyer): Form<CreateFlyerForm>,
+) -> AppResult<impl IntoResponse> {
     sqlx::query!(
-        r#"INSERT INTO flyers (user_id, x, y, image_url, event_url, event_name, event_end)
-           VALUES (?, ?, ?, ?, ?, ?, ?)"#,
+        r#"INSERT INTO flyers (user_id, x, y, image_url, flyer_name, remove_after_time)
+           VALUES (?, ?, ?, ?, ?, ?)"#,
         user.id,
         flyer.x,
         flyer.y,
         flyer.image_url,
-        flyer.event_url,
-        flyer.event_name,
-        flyer.event_end
+        flyer.flyer_name,
+        flyer.remove_after_time
     )
     .execute(&state.db)
     .await?;
 
-    tracing::info!("Created flyer!");
+    Ok(Redirect::to("/bulletin"))
+}
 
-    Ok(())
+#[derive(Debug, Serialize, Deserialize)]
+struct FlyerUpdate {
+    image_url: String,
+    flyer_name: String,
+    remove_after_time: NaiveDateTime,
 }
 
 async fn update_flyer(
     user: User,
     State(state): State<SharedAppState>,
     Path(id): Path<i64>,
-    Json(update): Json<FlyerForm>,
-) -> AppResult<()> {
+    Form(update): Form<FlyerUpdate>,
+) -> AppResult<impl IntoResponse> {
     if user.has_role(&state.db, User::ADMIN).await? {
         sqlx::query!(
             r#"UPDATE flyers
-               SET image_url = ?, event_name = ?, event_url = ?, event_end = ?
+               SET image_url = ?, flyer_name = ?, remove_after_time = ?
                WHERE id = ?"#,
             update.image_url,
-            update.event_name,
-            update.event_url,
-            update.event_end,
+            update.flyer_name,
+            update.remove_after_time,
             id,
         )
         .execute(&state.db)
@@ -73,12 +80,11 @@ async fn update_flyer(
     } else {
         sqlx::query!(
             r#"UPDATE flyers
-               SET image_url = ?, event_name = ?, event_url = ?, event_end = ?
+               SET image_url = ?, flyer_name = ?, remove_after_time = ?
                WHERE id = ? AND user_id = ?"#,
             update.image_url,
-            update.event_name,
-            update.event_url,
-            update.event_end,
+            update.flyer_name,
+            update.remove_after_time,
             id,
             user.id
         )
@@ -86,14 +92,21 @@ async fn update_flyer(
         .await?;
     }
 
-    Ok(())
+    Ok(Redirect::to("/bulletin"))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FlyerMoveUpdate {
+    x: i64,
+    y: i64,
+    rotation: i64,
 }
 
 async fn move_flyer(
     user: User,
     State(state): State<SharedAppState>,
     Path(id): Path<i64>,
-    Json(update): Json<Flyer>,
+    Json(update): Json<FlyerMoveUpdate>,
 ) -> AppResult<()> {
     if user.has_role(&state.db, User::ADMIN).await? {
         sqlx::query!(
@@ -137,32 +150,60 @@ async fn delete_flyer(user: User, State(state): State<SharedAppState>, Path(id):
     Ok(())
 }
 
+async fn flyer_details(
+    State(state): State<SharedAppState>,
+    Path(id): Path<i64>,
+) -> AppResult<impl IntoResponse> {
+    let flyer = sqlx::query_as!(
+        FlyerUpdate,
+        "SELECT flyer_name, image_url, remove_after_time FROM flyers WHERE id = ?",
+        id
+    )
+    .fetch_one(&state.db)
+    .await?;
+
+    Ok(Json(flyer))
+}
+
 async fn bulletin_page(
     user: Option<User>,
     State(state): State<SharedAppState>,
 ) -> AppResult<impl IntoResponse> {
     let flyers = sqlx::query_as!(
         Flyer,
-        "SELECT id, x, y, rotation, z_index, image_url, event_url, event_end FROM flyers"
+        "SELECT id, user_id, flyer_name, x, y, rotation, image_url, remove_after_time FROM flyers"
     )
     .fetch_all(&state.db)
     .await?;
 
+    let (editable_flyers, read_only_flyers) = if let Some(ref u) = user {
+        let is_admin = u.has_role(&state.db, User::ADMIN).await?;
+        if is_admin {
+            (flyers.into_iter().collect(), Vec::new())
+        } else {
+            flyers.into_iter().partition(|f| f.user_id == u.id)
+        }
+    } else {
+        (Vec::new(), flyers.into_iter().collect())
+    };
+
     #[derive(Template, WebTemplate)]
     #[template(path = "bulletin/index.html")]
     struct Html {
-        flyers: Vec<Flyer>,
+        read_only_flyers: Vec<Flyer>,
+        editable_flyers: Vec<Flyer>,
         user: Option<User>,
     }
 
-    Ok(Html { flyers, user })
+    Ok(Html { read_only_flyers, editable_flyers, user })
 }
 
 pub fn add_routes(router: AppRouter) -> AppRouter {
     router.public_routes(|r| {
         r.route("/bulletin", get(bulletin_page))
             .route("/bulletin/flyer/new", post(create_flyer))
-            .route("/bulletin/flyer/{id}", put(update_flyer).delete(delete_flyer))
+            .route("/bulletin/flyer/{id}", delete(delete_flyer).get(flyer_details))
+            .route("/bulletin/flyer/{id}/edit", post(update_flyer))
             .route("/bulletin/flyer/{id}/move", post(move_flyer))
     })
 }
