38d98737988917ea240feb495dd64f5a7dcbe627
Sam Wlody <swlody@gmail.com>
2025-10-02 21:09:14 -0400

    simple contact form

---
+++ src/app/contact.rs
@@ -0,0 +1,60 @@
+use lettre::Message;
+use lettre::message::Mailbox;
+use serde::Deserialize;
+
+use crate::prelude::*;
+
+pub fn add_routes(router: AppRouter) -> AppRouter {
+    router.public_routes(|r| r.route("/contact", get(contact_us_page).post(contact_us_form)))
+}
+
+async fn contact_us_page() -> AppResult<impl IntoResponse> {
+    #[derive(Template, WebTemplate)]
+    #[template(path = "contact/send.html")]
+    struct ContactUsTemplate;
+
+    Ok(ContactUsTemplate)
+}
+
+#[derive(Deserialize, Debug)]
+struct ContactForm {
+    name: String,
+    email: String,
+    subject: String,
+    message: String,
+}
+
+async fn contact_us_form(
+    State(state): State<SharedAppState>,
+    Form(form): Form<ContactForm>,
+) -> AppResult<impl IntoResponse> {
+    tracing::info!("Subitting contact form: {:?}", &form);
+
+    let name = if form.name.is_empty() {
+        Some("Anonymous".to_owned())
+    } else {
+        Some(form.name)
+    };
+
+    let mut message = Message::builder()
+        .from(Mailbox::new(
+            Some("Suggestion Box".to_owned()),
+            "noreply@lightandsound.design".parse().unwrap(),
+        ))
+        .to(Mailbox::new(
+            Some("Studio".to_owned()),
+            "studio@lightandsound.design".parse().unwrap(),
+        ))
+        .subject(form.subject);
+
+    if !form.email.is_empty() {
+        message = message.reply_to(Mailbox::new(name, form.email.parse().map_err(|_| AppError::BadRequest)?));
+    }
+    state.mailer.send(&message.body(form.message)?).await?;
+
+    #[derive(Template, WebTemplate)]
+    #[template(path = "contact/message_sent.html")]
+    struct MessageSentTemplate;
+
+    Ok(MessageSentTemplate)
+}
