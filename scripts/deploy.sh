c8f35bb86a685e90e45d34c58cd605ec081c7582
Jack Foltz <jack@foltz.io>
2025-10-06 21:51:12 -0400

    infra: flush WAL before backup/restore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,19 +5,21 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# Backup
-ssh $1 <<'EOS'
-TS=$(date +'%Y-%m-%d_%H-%M-%S')
-sudo cp -a /home/lsd/lsd /home/lsd/backups/lsd.$TS
-sudo ln -sfn /home/lsd/backups/lsd.$TS /home/lsd/backups/lsd.latest
-sudo cp -a /home/lsd/db.sqlite /home/lsd/backups/db.$TS.sqlite
-sudo ln -sfn /home/lsd/backups/db.$TS.sqlite /home/lsd/backups/db.latest.sqlite
-EOS
+rsync --rsync-path="sudo rsync" -Pavz target/aarch64-unknown-linux-gnu/release/lsd $1:/home/lsd/lsd.next
 
-# Deploy and restart
-rsync --rsync-path="sudo rsync" -Pavz target/aarch64-unknown-linux-gnu/release/lsd $1:/home/lsd/
 ssh $1 <<'EOS'
+sudo systemctl stop lsd
+sudo sqlite3 /home/lsd/db.sqlite "SELECT COUNT(*) FROM sqlite_master;" # flush the WAL
+
+TS=$(date +'%Y-%m-%d_%H-%M-%S')
+sudo cp -av /home/lsd/lsd /home/lsd/backups/lsd.$TS
+sudo cp -av /home/lsd/db.sqlite /home/lsd/backups/db.$TS.sqlite
+sudo ln -sfv /home/lsd/backups/lsd.$TS /home/lsd/backups/lsd.latest
+sudo ln -sfv /home/lsd/backups/db.$TS.sqlite /home/lsd/backups/db.latest.sqlite
+
+mv -v /home/lsd/lsd.next /home/lsd/lsd
 sudo setcap 'cap_net_bind_service=+ep' /home/lsd/lsd
-sudo systemctl restart lsd
+
+sudo systemctl start lsd
 sudo systemctl is-active --quiet lsd
 EOS
