7d1dc8b4098b57ecd1ff286d771367d57ac3375d
Jack Foltz <jack@foltz.io>
2025-10-06 21:51:12 -0400

    infra: flush WAL before backup/restore

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -5,10 +5,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-ssh $1 <<'EOS'
+#ssh $1 <<'EOS'
+ssh -i ~/.ssh/id_ed25519_lsd_root ec2-user@beta.lightandsound.design <<'EOS'
 sudo systemctl stop lsd
-sudo cp -aL /home/lsd/backups/lsd.latest /home/lsd/lsd
-sudo cp -aL /home/lsd/backups/db.latest.sqlite /home/lsd/db.sqlite
+sudo sqlite3 /home/lsd/db.sqlite "SELECT COUNT(*) FROM sqlite_master;" # flush the WAL
+sudo cp -aLv /home/lsd/backups/lsd.latest /home/lsd/lsd
+sudo cp -aLv /home/lsd/backups/db.latest.sqlite /home/lsd/db.sqlite
 sudo setcap 'cap_net_bind_service=+ep' /home/lsd/lsd
 sudo systemctl start lsd
 EOS
