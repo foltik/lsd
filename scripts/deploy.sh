5536c2919c77f572ef8478c7266c463ca370aa02
Jack Foltz <jack@foltz.io>
2025-10-06 22:09:13 -0400

    infra: new WAL flush method, backup pre-rollback, set -euxo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,17 @@ fi
 rsync --rsync-path="sudo rsync" -Pavz target/aarch64-unknown-linux-gnu/release/lsd $1:/home/lsd/lsd.next
 
 ssh $1 <<'EOS'
+set -euxo pipefail
 sudo systemctl stop lsd
-sudo sqlite3 /home/lsd/db.sqlite "SELECT COUNT(*) FROM sqlite_master;" # flush the WAL
+sudo sqlite3 /home/lsd/db.sqlite "PRAGMA wal_checkpoint(TRUNCATE);"
 
 TS=$(date +'%Y-%m-%d_%H-%M-%S')
-sudo cp -av /home/lsd/lsd /home/lsd/backups/lsd.$TS
-sudo cp -av /home/lsd/db.sqlite /home/lsd/backups/db.$TS.sqlite
-sudo ln -sfv /home/lsd/backups/lsd.$TS /home/lsd/backups/lsd.latest
-sudo ln -sfv /home/lsd/backups/db.$TS.sqlite /home/lsd/backups/db.latest.sqlite
+sudo cp -a /home/lsd/lsd /home/lsd/backups/lsd.$TS
+sudo cp -a /home/lsd/db.sqlite /home/lsd/backups/db.$TS.sqlite
+sudo ln -sf /home/lsd/backups/lsd.$TS /home/lsd/backups/lsd.latest
+sudo ln -sf /home/lsd/backups/db.$TS.sqlite /home/lsd/backups/db.latest.sqlite
 
-mv -v /home/lsd/lsd.next /home/lsd/lsd
+sudo mv /home/lsd/lsd.next /home/lsd/lsd
 sudo setcap 'cap_net_bind_service=+ep' /home/lsd/lsd
 
 sudo systemctl start lsd
