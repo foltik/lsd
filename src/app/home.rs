f2a5a941adc984b480ffbbace641c7ff5e48a958
Jack Foltz <jack@foltz.io>
2025-12-22 21:58:01 -0500

    home: basic dashboard via long pressing logo

--- src/app/home.rs
+++ src/app/home.rs
@@ -3,7 +3,10 @@ use crate::prelude::*;
 
 /// Add all `home` routes to the router.
 pub fn add_routes(router: AppRouter) -> AppRouter {
-    router.public_routes(|r| r.route("/", get(home_page)).route("/past", get(past_page)))
+    router
+        .public_routes(|r| r.route("/", get(home_page)).route("/past", get(past_page)))
+        // TODO: Rethink roles, not WRITER. Template out buttons based on role.
+        .restricted_routes(User::WRITER, |r| r.route("/dashboard", get(dashboard_page)))
 }
 
 #[derive(Template, WebTemplate)]
@@ -14,6 +17,12 @@ struct HomeHtml {
     past: bool,
 }
 
+#[derive(Template, WebTemplate)]
+#[template(path = "dashboard.html")]
+struct DashboardHtml {
+    user: Option<User>,
+}
+
 /// Display the front page.
 async fn home_page(user: Option<User>, State(state): State<SharedAppState>) -> HtmlResult {
     Ok(HomeHtml { user, events: Event::list_upcoming(&state.db).await?, past: false }.into_response())
@@ -21,4 +30,8 @@ async fn home_page(user: Option<User>, State(state): State<SharedAppState>) -> H
 
 async fn past_page(user: Option<User>, State(state): State<SharedAppState>) -> HtmlResult {
     Ok(HomeHtml { user, events: Event::list_past(&state.db).await?, past: true }.into_response())
+}
+
+async fn dashboard_page(user: User) -> HtmlResult {
+    Ok(DashboardHtml { user: Some(user) }.into_response())
 }
