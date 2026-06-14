b7147e20602155387794dc3582e3a67ed92c53da
Jack Foltz <jack@foltz.io>
2026-06-14 19:54:11 -0400

    perf: add various user_id db indexes

---
+++ migrations/0044_add_user_id_indexes.sql
@@ -0,0 +1,4 @@
+CREATE INDEX transactions_user_id ON transactions(user_id);
+CREATE INDEX rsvps_user_id ON rsvps(user_id);
+CREATE INDEX rsvp_sessions_user_id ON rsvp_sessions(user_id);
+CREATE INDEX manual_rsvps_user_id ON manual_rsvps(user_id);
